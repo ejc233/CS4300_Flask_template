0d366ecc203cee29147417a31a178fcbc4404fd8
Xinyu Zhao <xinyuzhao23@gmail.com>
2018-04-22 14:23:47 -0400

    started modal

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
   $("#myCarousel").carousel({interval: false, wrap: false});
+
+  $(".modal").map(function () {
+    $(this).detach().appendTo($('body'));
+  })
+
+  $(".poster").click(function(){
+    var id = this.getAttribute("data-movie");
+    // var row = $(this).parent().parent();
+    var mod = $('body').find("#"+id);
+
+    // mod.detach().appendTo($('body'));
+
+    mod.show("medium");
+    // console.log(mod);
+  });
+
+  $(".post_title").click(function(){
+    var id = this.getAttribute("data-movie");
+    // var row = $(this).parent().parent();
+    var mod = $('body').find("#"+id);
+
+    // mod.detach().appendTo($('body'));
+
+    mod.show(400);
+    // console.log(mod);
+  });
+
+  window.onclick = function(event) {
+    console.log(event.target);
+    if (event.target.className == "modal") {
+        $(event.target).hide(400);
+    }
+  }
 });
